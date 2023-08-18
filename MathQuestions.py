19bd21b1824d8694e00061778924cb040dd5412a
kyleCS50 <kyledanielfrancis@gmail.com>
2023-08-18 17:12:21 +1200

    Added timer

--- MathQuestions.py
+++ MathQuestions.py
@@ -17,39 +17,24 @@ class MathQuestions(threading.Thread):
         self.correct = 0
         self.userAnswer = ""
         self.gameOver = False
+        self.current_time = 0
+        self.max_time = 15
 
     def run(self):
         self.total = len(self.questions)
         for i in range(self.total):
+            self.current_time = time.time()
             self.question = self.questions[i]
             self.answer = self.answers[i]
             while self.userAnswer != self.answer:
                 time.sleep(0.5)
-            self.correct += 1
+                if time.time() - self.current_time > self.max_time:
+                    break
+            if self.userAnswer == self.answer:
+                self.correct += 1
             time.sleep(0.5)
         self.score = self.correct / self.total * 100
         self.gameOver = True
-
-    def getScore(self):
-        return self.score
-
-    def getCorrect(self):
-        return self.correct
-
-    def getTotal(self):
-        return self.total
-
-    def getQuestion(self):
-        return self.question
-
-    def getAnswer(self):
-        return self.answer
-
-    def getQuestions(self):
-        return self.questions
-
-    def getAnswers(self):
-        return self.answers
 
 
 def main():
@@ -61,13 +46,15 @@ def main():
     math.start()
     while True:
         success, img = cap.read()
+        h, w, c = img.shape
         image = cv2.flip(img, 1)
         image = detector.find_hands(image, draw=False)
         if not math.gameOver:
-            cv2.putText(image, math.getQuestion(), (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 5)
+            cv2.putText(image, "Time Remaining: " + str(math.max_time - int(time.time() - math.current_time)), (10, h - 10), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 5)
+            cv2.putText(image, math.question, (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 5)
             math.userAnswer = str(detector.count_fingers(image))
             cv2.putText(image, math.userAnswer, (800, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 5)
-            if math.userAnswer == math.getAnswer():
+            if math.userAnswer == math.answer:
                 cv2.putText(image, "Correct!", (10, 150), cv2.FONT_HERSHEY_PLAIN, 5, (0, 255, 0), 5)
         else:
             cv2.putText(image, "Score: " + str(math.correct / math.total * 100) + "%", (10, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 5)
