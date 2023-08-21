bb16d497aa44df51d22c87e46ab6fe10adaf9a51
kyleCS50 <kyledanielfrancis@gmail.com>
2023-08-21 13:43:32 +1200

    Added quit or play again options after game is over

--- MathQuestions.py
+++ MathQuestions.py
@@ -13,35 +13,35 @@ class MathQuestions(threading.Thread):
         # dictionary with answer keys and question values for each operation
         self.answers_and_questions = {
             "1": ["What is 0 + 1?", "What is 3 - 2?", "What is 1 x 1?", "What is 2 / 2?"],
-            "2": ["What is 1 + 1?", "What is 5 - 3?", "What is 2 x 1?", "What is 4 / 2?"],
-            "3": ["What is 1 + 2?", "What is 7 - 4?", "What is 3 x 1?", "What is 9 / 3?"],
-            "4": ["What is 2 + 2?", "What is 9 - 5?", "What is 2 x 2?", "What is 24 / 6?"],
-            "5": ["What is 2 + 3?", "What is 11 - 6?", "What is 5 x 1?", "What is 15 / 3?"],
-            "6": ["What is 3 + 3?", "What is 13 - 7?", "What is 2 x 3?", "What is 54 / 9?"],
-            "7": ["What is 5 + 2?", "What is 16 - 9?", "What is 7 x 1?", "What is 56 / 8?"],
-            "8": ["What is 4 + 4?", "What is 19 - 11?", "What is 2 x 4?", "What is 32 / 4?"],
-            "9": ["What is 5 + 4?", "What is 21 - 12?", "What is 3 x 3?", "What is 81 / 9?"],
-            "10": ["What is 5 + 5?", "What is 25 - 15?", "What is 2 x 5?", "What is 100 / 10?"],
+            # "2": ["What is 1 + 1?", "What is 5 - 3?", "What is 2 x 1?", "What is 4 / 2?"],
+            # "3": ["What is 1 + 2?", "What is 7 - 4?", "What is 3 x 1?", "What is 9 / 3?"],
+            # "4": ["What is 2 + 2?", "What is 9 - 5?", "What is 2 x 2?", "What is 24 / 6?"],
+            # "5": ["What is 2 + 3?", "What is 11 - 6?", "What is 5 x 1?", "What is 15 / 3?"],
+            # "6": ["What is 3 + 3?", "What is 13 - 7?", "What is 2 x 3?", "What is 54 / 9?"],
+            # "7": ["What is 5 + 2?", "What is 16 - 9?", "What is 7 x 1?", "What is 56 / 8?"],
+            # "8": ["What is 4 + 4?", "What is 19 - 11?", "What is 2 x 4?", "What is 32 / 4?"],
+            # "9": ["What is 5 + 4?", "What is 21 - 12?", "What is 3 x 3?", "What is 81 / 9?"],
+            # "10": ["What is 5 + 5?", "What is 25 - 15?", "What is 2 x 5?", "What is 100 / 10?"],
         }
         self.current_question = ""
         self.current_answer = ""
-        self.total_questions = 40
+        self.total_questions = sum([len(self.answers_and_questions[key]) for key in self.answers_and_questions.keys()])
         self.correct_answers = 0
         self.user_answer = ""
         self.game_over = False
         self.current_time = 0
         self.max_time = 15
-        self.question_number = 0
+        self.question_number = 1
         self.number_fact = ""
 
     def run(self):
-        for i in range(self.total_questions):
-            self.question_number = i + 1
+        while not self.game_over:
             if self.answers_and_questions == {}:
+                print("No more questions")
                 break
             number_of_questions = 0
             while number_of_questions == 0:
-                random_answer = random.randint(1, 10)
+                random_answer = random.randint(1, len(self.answers_and_questions.keys()))
                 self.current_answer = str(random_answer)
                 if self.current_answer in self.answers_and_questions.keys():
                     number_of_questions = len(self.answers_and_questions[self.current_answer])
@@ -49,6 +49,7 @@ class MathQuestions(threading.Thread):
             self.current_question = self.answers_and_questions[self.current_answer][random_question]
             del self.answers_and_questions[self.current_answer][random_question]
             if not self.answers_and_questions[self.current_answer]:
+                print("No more questions for " + self.current_answer)
                 del self.answers_and_questions[self.current_answer]
             self.current_time = time.time()
             self.number_fact = facts.FactAPI(self.current_answer).get_fact()
@@ -63,6 +64,7 @@ class MathQuestions(threading.Thread):
                     break
             if self.user_answer == self.current_answer:
                 self.correct_answers += 1
+            self.question_number += 1
             time.sleep(0.5)
         self.score = self.correct_answers / self.total_questions * 100
         self.game_over = True
@@ -103,9 +105,21 @@ def main():
             cv2.putText(image, "Score: " + str(math.correct_answers / math.total_questions * 100) + "%", (10, 70),
                         cv2.FONT_HERSHEY_PLAIN,
                         5, (255, 0, 255), 5)
+            # instructions for hold up 5 fingers on left hand to play again or 5 fingers on right hand to quit
+            cv2.putText(image, "Hold up 5 fingers on your left hand to play again", (10, 150), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+            cv2.putText(image, "Hold up 5 fingers on your right hand to quit", (10, 200), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+            if detector.count_fingers(image) == 5 and detector.hand_side == "Left":
+                math = MathQuestions()
+                math.start()
+            elif detector.count_fingers(image) == 5 and detector.hand_side == "Right":
+                math.join()
+                break
+
         cv2.imshow("Output", image)
         cv2.waitKey(1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            math.game_over = True
+            math.join()
             break
 
 
